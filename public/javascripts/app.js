fd58280326b88c51c80a6ed4c530c76e0ebb9dde
Taylor <taylor.hardy177@gmail.com>
2014-11-24 13:05:54 -0600

    changed template

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,6 +5,18 @@ app.config(['$routeProvider', function($routeProvider){
         when('/home', {
             templateUrl: 'partials/home.html'
         }).
+        when('/aboutus', {
+            templateUrl: 'partials/construction.html'
+        }).
+        when('/projects', {
+            templateUrl: 'partials/construction.html'
+        }).
+        when('/location', {
+            templateUrl: 'partials/construction.html'
+        }).
+        when('/contact', {
+            templateUrl: 'partials/construction.html'
+        }).
         otherwise({
             redirectTo: '/home'
         });
